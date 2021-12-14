9c8dcebf8dc2359e021f2372c309d473f3509e2e
vogonwann <wannjanjic@gmail.com>
2021-12-14 21:54:11 +0100

    first commit

---
+++ ITA.Wad.Cars/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ITA.Wad.Cars.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITA.Wad.Cars.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
---
+++ ITA.Wad.Cars/Data/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITA.Wad.Cars.Data.Models
+{
+    public class Car
+    {
+    }
+}
